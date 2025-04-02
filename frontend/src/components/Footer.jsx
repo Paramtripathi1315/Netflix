eccee0f147f1e2b980f5b231bb317454beed5a41
Param Tripathi <paramt1315@gmail.com@gamil.com>
2025-04-02 13:19:23 +0530

    index

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
           </a>
           . The source code is available on{" "}
           <a
-            href="https://github.com/Paramtripathi1315/Netflix-clone"
+            href="https://github.com/Paramtripathi1315/Netflix"
             target="_blank"
             rel="noreferrer"
             className="font-medium underline underline-offset-4"
