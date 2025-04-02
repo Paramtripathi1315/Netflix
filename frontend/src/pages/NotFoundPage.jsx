3fd637f7411ff1ec9ac24eaecdbb34677cdc154b
Param Tripathi <paramt1315@gmail.com@gamil.com>
2025-04-02 12:27:51 +0530

    index.css hero bg fixed

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 const NotFoundPage = () => {
   return (
     <div
-      className="min-h-screen bg-cover bg-center flex flex-col justify-center items-center text-white"
+      className="h-screen bg-cover bg-center flex flex-col justify-center items-center text-white relative"
       style={{ backgroundImage: `url('/404.png')` }}
     >
       <header className="absolute top-0 l0 p-4 bg-black w-full">
@@ -12,7 +12,7 @@ const NotFoundPage = () => {
       </header>
       <main className="text-center error-page--content z-10">
         <h1 className="text-7xl font-semibold mb-4">Lost your way?</h1>
-        <p className="mb-6 text-xl">
+        <p className="mb-6 text-xl max-w-lg">
           Sorry, we can't find that page. You'll find lots to explore on the
           home page.
         </p>
